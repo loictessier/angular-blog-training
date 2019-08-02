0ee89bb272c066fa39dd16d22a4d2744f590705a
Loïc TESSIER <loictessier.inge@gmail.com>
2019-08-02 14:51:50 +0200

    Reorganize components tree : moved post-list-item component to components/post-list/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PostListComponent } from './components/post-list/post-list.component';
-import { PostListItemComponent } from './components/post-list-item/post-list-item.component';
+import { PostListItemComponent } from './components/post-list/post-list-item/post-list-item.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PostListComponent,
-    PostListItemComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        PostListComponent,
+        PostListItemComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
